d7874c7bbeaf1c9e0f11cf9c2beece34e096cd61
Danil Volkov <volchara287@gmail.com>
2023-03-03 16:54:10 +0600

    Выполнил второй шаг проекта

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-import program from "../src/gendiff.js";
+import program from "../src/cmd/gendiff.js";
 
 program.parse();
--- src/cmd/gendiff.js
+++ src/cmd/gendiff.js
@@ -5,5 +5,7 @@ const program = new Command();
 program
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --file <type>', 'Choose type file')
 
 export default program;
