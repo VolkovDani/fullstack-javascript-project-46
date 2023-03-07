e2910b1867035fbad5c2cd1da0120a6d55dc15d1
Danil Volkov <volchara287@gmail.com>
2023-03-08 03:09:51 +0600

    Подготовка jest

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,8 +1,18 @@
-import { genDiff } from "../src/genDiff"
+import { genDiff } from "../src/genDiff.js"
+import { test } from "@jest/globals"
 
-const firstFile = '__tests__/res/file1.json'
+const firstFile = '__tests__/res/file.json'
 const secondFile = '__tests__/res/file2.json'
+const thirdFile = '/home/danil/Projects Programming/fullstack-javascript-project-46/__tests__/res/file1.json'
 
 test('Test 1', () => {
-  expect(genDiff(firstFile, secondFile))
+  expect(genDiff(thirdFile, secondFile)).toBe(`{
+    - follow: false
+      host: hexlet.io
+    - proxy: 123.234.53.22
+    - timeout: 50
+    + timeout: 20
+    + verbose: true
+  }`)
 })
+
