13c0c44e83b81c9f8b33d106a8b66d4979beca7b
Danil Volkov <volchara287@gmail.com>
2023-03-08 03:14:57 +0600

    Создание json из str

--- src/genDiff.js
+++ src/genDiff.js
@@ -1,12 +1,35 @@
 import { readFileSync } from 'node:fs'
+import path from 'node:path';
+
+const makejson = (filepath) => {
+  let str = '';
+  try {
+    str = readFileSync(path.resolve(filepath), 'utf-8', (err) => {
+    if (err) throw err;
+  });
+  } catch (e) {
+    console.log(e.name + ': ' + e.message);
+  }
+  
+  return JSON.parse(str);
+}
 
 const genDiff = (filepath1, filepath2) => {
   
+    const firstFile = makejson(filepath1);
+    const secondFile = makejson(filepath2);
+
+    console.log(firstFile);
+    console.log(secondFile);
+  
+  return 0;
 };
 
-console.log(readFileSync('__tests__/res/file1.json', 'utf-8', (err, data) => {
-  if (err) throw err;
-  console.log(data);
-})); 
+
+// test section
+const firstFile = '__tests__/res/file1.json'
+const secondFile = '__tests__/res/file2.json'
+
+differenceJSON(makejson(firstFile))
 
 export { genDiff };
