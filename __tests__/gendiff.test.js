075f9d8a085ae0bf1790b240107e619fa9414632
Danil Volkov <volchara287@gmail.com>
2023-03-20 12:16:03 +0600

    Удалены константы с ссылками на файлы

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,9 +1,8 @@
 import { genDiff } from "../src/genDiff.js";
 import { test, expect } from "@jest/globals";
 
-const firstFile = "./__fixtures__/file1.json";
-const secondFile = "./__fixtures__/file2.json";
-const finalJSON = `{
+
+const finalResult = `{
   - follow: false
     host: hexlet.io
   - proxy: 123.234.53.22
@@ -12,9 +11,13 @@ const finalJSON = `{
   + verbose: true
 }`;
 
-test("Test 1", () => {
-  expect(genDiff(firstFile, secondFile)).toStrictEqual(finalJSON);
+test("Test JSON", () => {
+  expect(genDiff("./__fixtures__/file1.json", "./__fixtures__/file2.json")).toStrictEqual(finalResult);
 });
+
+test("Test YAML", () => {
+  expect(genDiff("./__fixtures__/file1.yml", "./__fixtures__/file2.yaml")).toStrictEqual(finalResult)
+})
 
 test("Test throw err", () => {
   expect(genDiff).toThrow("Пустой путь к файлу");
