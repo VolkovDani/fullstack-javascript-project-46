d6d82bdb0cee616e5039614caea2343006609fde
Danil Volkov <volchara287@gmail.com>
2023-04-11 20:48:56 +0600

    Корректное построение дифа с плоскими

--- utils/genStrWithDiff.js
+++ utils/genStrWithDiff.js
@@ -1,15 +1,16 @@
 // Принимает массив объектов с описанием ключей[{ключ, значение, значение "разности файлов"},...]
 // Возвращает строку в формате "{ *значение дифа* ключ: значение }"
-const genStrWithDiffs = (arr) => {
-  const arrWithSort = Object.assign([], arr);
+const genStrWithDiffs = (arr, deep = 0) => {
+  const arrWithObj = Object.assign([], arr);
   let str = `{\n`
-  arrWithSort.sort(({key: key1}, {key: key2}) => {
+  arrWithObj.sort(({key: key1}, {key: key2}) => {
     if (key1 < key2) return -1;
     if (key1 > key2) return 1;
     if (key1 == key2) return 0;
   }).map(({key, value, diff}) => {
-    if (diff == '=') str += '    ';
-    else str += `  ${diff} `
+    if (typeof value == 'object') str += genStrWithDiffs(value, deep++)
+    if (diff == '=') str += `${' '.repeat(4 + deep)}`;
+    else str += `${' '.repeat(2 + deep)}${diff} `
     str += `${key}: ${value}\n`
   })
   str += '}'
