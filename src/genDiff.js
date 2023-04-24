7c289da45789257c7bd74424df4f596c613979d7
Danil Volkov <volchara287@gmail.com>
2023-04-24 16:47:04 +0600

    Код проходит все тесты

--- src/genDiff.js
+++ src/genDiff.js
@@ -1,87 +1,34 @@
-// import _ from "lodash";
+import _ from "lodash";
 import parceFile from "../utils/parcers.js";
 import genStrWithDiffs from "../utils/genStrWithDiff.js";
 
-// Эта функция получает на вход два объекта
-// На выходе получаем третий объект с описанием разницы первых двух
-const getArrDifferenceFiles = (object1, object2) => {
-  const mainAccumulator = [];
-  const prepareKeysFirstObj = (obj1, obj2, accumulator) => {
-    Object.entries(obj1).reduce((acc, [key, value]) => {
-      const innerObj = {};
-      innerObj.key = key;
-      innerObj.value = value;
-      if (Object.hasOwn(obj2, key)) {
-        if (obj2[key] == value) {
-          innerObj.diff = "=";
-        } else {
-          if (typeof value == "object") {
-            if (typeof obj2[key] == "object") innerObj.diff = "=";
-            else {
-              innerObj.diff = "-";
-            }
-            innerObj.value = prepareKeysFirstObj(value, obj2[key], []);
-          } else innerObj.diff = "-";
-        }
-      } else {
-        innerObj.diff = "-";
-      }
-      acc.push(innerObj);
-      return acc;
-    }, accumulator);
-    return accumulator;
-  };
-
-  prepareKeysFirstObj(object1, object2, mainAccumulator);
-
-  const prepareKeysSecondObj = (obj2, accumulator) => {
-    // console.log(obj2);
-    Object.entries(obj2).reduce((acc, [key, value]) => {
-      const innerObj = {};
-      if (
-        value != null &&
-        typeof value == "object"
-      ) {
-        const innerAcc =
-          acc[
-            acc
-              .reduce((acc, { key: name }) => {
-                acc.push(name);
-                return acc;
-              }, [])
-              .indexOf(key)
-          ];
-        if (innerAcc)
-          innerObj.value = prepareKeysSecondObj(value, innerAcc.value);
-        return acc;
-      } else innerObj.value = value;
-      innerObj.key = key;
-      const findedKey = acc.find(({ key: keyInObj }) => keyInObj == key);
-      if (findedKey) {
-        if (findedKey.value != value) {
-          innerObj.diff = "+";
-          if (typeof value == "object") innerObj.diff = "=";
-          acc.push(innerObj);
-        }
-      } else {
-        innerObj.diff = "+";
-        acc.push(innerObj);
-      }
-      return acc;
-    }, accumulator);
-    return accumulator;
-  };
-
-  prepareKeysSecondObj(object2, mainAccumulator);
-  return mainAccumulator;
+const makeAstTree = (mainObj1, mainObj2) => {
+  const keys = _.sortBy(_.union(_.keys(mainObj1), _.keys(mainObj2)))
+  const treePart = keys.map((key) => {
+    if (!_.has(mainObj1, key)) return {key, 'value': mainObj2[key], 'status': 'added'}
+    if (!_.has(mainObj2, key)) return {key, 'value': mainObj1[key], 'status': 'deleted'}
+    if (mainObj1[key] == mainObj2[key]) return {key, 'value': mainObj1[key], 'status': 'equal'}
+    if (_.isObject(mainObj1[key]) && _.isObject(mainObj2[key])) return {key, 'children': makeAstTree(mainObj1[key], mainObj2[key]), 'status': 'nested'}
+    if (mainObj1[key] != mainObj2[key])return {key, 'newValue': mainObj2[key], 'oldValue': mainObj1[key]}
+  })
+  return treePart
 };
+
+
 
 const genDiff = (filepath1, filepath2) => {
   return genStrWithDiffs(
-    getArrDifferenceFiles(parceFile(filepath1), parceFile(filepath2))
-  );
-};
+    makeAstTree(parceFile(filepath1), parceFile(filepath2))
+    );
+  };
+  
+  
+const firstObj = ("./__fixtures__/file3.json");
+const secondObj = ("./__fixtures__/file4.json");
+  // console.log(JSON.stringify(getAllKeys(firstObj, secondObj)));
+  // console.log(buildAstTree(firstObj, secondObj));
+  // console.log(JSON.stringify(makeAstTree(firstObj, secondObj)));
 
-// console.log(getArrDifferenceFiles(parceFile("./__fixtures__/file1.json"), parceFile("./__fixtures__/file2.json")));
-console.log(genDiff("./__fixtures__/file3.json", "./__fixtures__/file4.json"));
+  // console.log(getArrDifferenceFiles(parceFile("./__fixtures__/file1.json"), parceFile("./__fixtures__/file2.json")));
+console.log(genDiff(firstObj, secondObj));
 export default genDiff;
--- utils/genStrWithDiff.js
+++ utils/genStrWithDiff.js
@@ -1,24 +1,67 @@
-// Принимает массив объектов с описанием ключей[{ключ, значение, значение "разности файлов"},...]
-// Возвращает строку в формате "{ *значение дифа* ключ: значение }"
+import _ from "lodash";
 
+const genStrWithDiffs = (arr) => {
+  let str = `{\n`;
+  const prepareObjValue = (child, deep = 0) => {
+    if (!_.isObject(child)) return `${child}\n`;
+    let newStrPre = "{\n";
+    const templateSpace = " ".repeat(4 + 4 * deep);
+    const templateSpaceForBracket = " ".repeat(4 * deep);
+    _.keys(child).map((key) => {
+      newStrPre += `${templateSpace}${key}: `;
+      if (_.isObject(child[key])) {
+        newStrPre += prepareObjValue(child[key], deep + 1);
+      } else newStrPre += `${child[key]}\n`;
+    });
+    newStrPre += `${templateSpaceForBracket}}\n`;
+    return newStrPre;
+  };
 
-const genStrWithDiffs = (arr, deep = 0) => {
-  const arrWithObj = Object.assign([], arr);
-  let str = `{\n`
-  arrWithObj.sort(({key: key1}, {key: key2}) => {
-    if (key1 < key2) return -1;
-    if (key1 > key2) return 1;
-    if (key1 == key2) return 0;
-  }).map(({key, value, diff}) => {
-    let objValue = ''
-    if (typeof value == 'object') objValue = genStrWithDiffs(value, deep + 1)
-    if (diff == '=') str += `${' '.repeat(4 + deep)}`;
-    else str += `${' '.repeat(2 + deep)}${diff} `
-    str += `${key}: ${objValue || value}\n`
-    // console.log(str);
-  })
-  str += `${' '.repeat(deep)}}`
+  const genStrChild = (child, deep = 0) => {
+    let newStr = "";
+    child.map(({ key, children, status, value, newValue, oldValue }) => {
+      let strObj = "";
+      const templateSpace = " ".repeat(2 + 4 * deep);
+      if (newValue || oldValue) {
+        strObj +=
+          `${templateSpace}- ` +
+          `${key}: ${prepareObjValue(oldValue, deep + 1)}`.trim();
+        strObj += "\n";
+        strObj +=
+          `${templateSpace}+ ` +
+          `${key}: ${prepareObjValue(newValue, deep + 1)}`.trim();
+        strObj += "\n";
+      }
+      if (status == "nested") {
+        strObj += `${templateSpace}  ${key}: {\n`;
+        strObj += genStrChild(children, deep + 1);
+        strObj += `${templateSpace}  }\n`;
+      }
+      if (status == "deleted") {
+        strObj += `${templateSpace}- ${key}: ${prepareObjValue(
+          value,
+          deep + 1
+        )}`;
+      }
+      if (status == "added") {
+        if (_.isObject(value)) {
+          strObj += `${templateSpace}+ ${key}: `;
+          strObj += prepareObjValue(value, deep + 1);
+        } else strObj += `${templateSpace}+ ${key}: ${value}\n`;
+      }
+      if (status == "equal") {
+        if (_.isObject(value)) {
+          strObj += `${templateSpace}  ${key}: `;
+          strObj += prepareObjValue(value, deep + 1);
+        } else strObj += `${templateSpace}  ${key}: ${value}\n`;
+      }
+      return (newStr += strObj);
+    });
+    return newStr;
+  };
+  str += genStrChild(arr);
+  str += "}";
   return str;
-}
+};
 
 export default genStrWithDiffs;
