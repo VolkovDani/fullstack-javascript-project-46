dab567e117e3c1ef2a182e7b09cef3a1897076af
Danil Volkov <volchara287@gmail.com>
2023-05-02 21:16:48 +0600

    Убрал let из stylish

--- src/formatters/formats/stylish.js
+++ src/formatters/formats/stylish.js
@@ -1,68 +1,54 @@
 import _ from 'lodash';
 
-
 const genStr = (arr, spaceStr) => {
-  let str = `{\n`;
   const prepareObjValue = (child, deep = 0) => {
     if (!_.isObject(child)) return `${child}\n`;
-    let newStrPre = '{\n';
     const templateSpace = String(spaceStr).repeat(4 + 4 * deep);
     const templateSpaceForBracket = String(spaceStr).repeat(4 * deep);
-    _.keys(child).map((key) => {
-      newStrPre += `${templateSpace}${key}: `;
+    return '{\n' + _.keys(child).map((key) => {
       if (_.isObject(child[key])) {
-        newStrPre += prepareObjValue(child[key], deep + 1);
-      } else newStrPre += `${child[key]}\n`;
-    });
-    newStrPre += `${templateSpaceForBracket}}\n`;
-    return newStrPre;
+        return `${templateSpace}${key}: ` + prepareObjValue(child[key], deep + 1);
+      } else return `${templateSpace}${key}: ` + `${child[key]}\n`;
+    }).join('') + `${templateSpaceForBracket}}\n`
   };
 
   const genStrChild = (child, deep = 0) => {
-    let newStr = '';
-    child.map(({ key, children, status, newValue, oldValue }) => {
-      let strObj = '';
-      const templateSpace = String(spaceStr).repeat(2 + 4 * deep);
-      if (newValue || oldValue) {
-        strObj +=
-          `${templateSpace}- ` +
-          `${key}: ${prepareObjValue(oldValue, deep + 1)}`.trim();
-        strObj += '\n';
-        strObj +=
-          `${templateSpace}+ ` +
-          `${key}: ${prepareObjValue(newValue, deep + 1)}`.trim();
-        strObj += '\n';
-      }
-      if (status == 'nested') {
-        strObj += `${templateSpace}  ${key}: {\n`;
-        strObj += genStrChild(children, deep + 1);
-        strObj += `${templateSpace}  }\n`;
-      }
-      if (status == 'deleted') {
-        strObj += `${templateSpace}- ${key}: ${prepareObjValue(
-          children,
-          deep + 1
-        )}`;
-      }
-      if (status == 'added') {
-        if (_.isObject(children)) {
-          strObj += `${templateSpace}+ ${key}: `;
-          strObj += prepareObjValue(children, deep + 1);
-        } else strObj += `${templateSpace}+ ${key}: ${children}\n`;
-      }
-      if (status == 'equal') {
-        if (_.isObject(children)) {
-          strObj += `${templateSpace}  ${key}: `;
-          strObj += prepareObjValue(children, deep + 1);
-        } else strObj += `${templateSpace}  ${key}: ${children}\n`;
-      }
-      return (newStr += strObj);
-    });
-    return newStr;
+    return child
+      .map(({ key, children, status, newValue, oldValue }) => {
+        const templateSpace = String(spaceStr).repeat(2 + 4 * deep);
+        if (newValue || oldValue) {
+          return (
+            `${templateSpace}- ` +
+            `${key}: ${prepareObjValue(oldValue, deep + 1)}`.trim() +
+            '\n' +
+            `${templateSpace}+ ` +
+            `${key}: ${prepareObjValue(newValue, deep + 1)}`.trim() +
+            '\n'
+          );
+        }
+        if (status == 'nested') {
+          return `${templateSpace}  ${key}: {\n` + genStrChild(children, deep + 1) + `${templateSpace}  }\n`;
+        }
+        if (status == 'deleted') {
+          return `${templateSpace}- ${key}: ${prepareObjValue(
+            children,
+            deep + 1
+          )}`;
+        }
+        if (status == 'added') {
+          if (_.isObject(children)) {
+            return `${templateSpace}+ ${key}: ` + prepareObjValue(children, deep + 1);
+          } else return `${templateSpace}+ ${key}: ${children}\n`;
+        }
+        if (status == 'equal') {
+          if (_.isObject(children)) {
+            return `${templateSpace}  ${key}: ` + prepareObjValue(children, deep + 1);
+          } else return `${templateSpace}  ${key}: ${children}\n`;
+        }
+      })
+      .join('');
   };
-  str += genStrChild(arr);
-  str += '}';
-  return str;
+  return '{\n' + genStrChild(arr) + '}';
 };
 
 export default genStr;
