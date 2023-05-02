7a3a346efddfbe88a54d26587e816d5ed770bb2e
Danil Volkov <volchara287@gmail.com>
2023-05-02 20:08:29 +0600

    Вынесение makeAstTree в отдельный файл

--- src/genDiff.js
+++ src/genDiff.js
@@ -1,28 +1,6 @@
-import _ from 'lodash';
 import parceFile from './utils/parcers.js';
 import setFormat from './formatters/index.js';
-
-
-const makeAstTree = (mainObj1, mainObj2) => {
-  const keys = _.sortBy(_.union(_.keys(mainObj1), _.keys(mainObj2)));
-  const treePart = keys.map((key) => {
-    if (!_.has(mainObj1, key))
-      return { key, value: mainObj2[key], status: 'added' };
-    if (!_.has(mainObj2, key))
-      return { key, value: mainObj1[key], status: 'deleted' };
-    if (mainObj1[key] == mainObj2[key])
-      return { key, value: mainObj1[key], status: 'equal' };
-    if (_.isObject(mainObj1[key]) && _.isObject(mainObj2[key]))
-      return {
-        key,
-        children: makeAstTree(mainObj1[key], mainObj2[key]),
-        status: 'nested',
-      };
-    if (mainObj1[key] != mainObj2[key])
-      return { key, newValue: mainObj2[key], oldValue: mainObj1[key], status: 'updated'};
-  });
-  return treePart;
-};
+import makeAstTree from './makeAstTree.js';
 
 
 const genDiff = (filepath1, filepath2, formatOutput = 'stylish') => {
---
+++ src/makeAstTree.js
@@ -0,0 +1,24 @@
+import _ from 'lodash';
+
+const makeAstTree = (mainObj1, mainObj2) => {
+  const keys = _.sortBy(_.union(_.keys(mainObj1), _.keys(mainObj2)));
+  const treePart = keys.map((key) => {
+    if (!_.has(mainObj1, key))
+      return { key, value: mainObj2[key], status: 'added' };
+    if (!_.has(mainObj2, key))
+      return { key, value: mainObj1[key], status: 'deleted' };
+    if (mainObj1[key] == mainObj2[key])
+      return { key, value: mainObj1[key], status: 'equal' };
+    if (_.isObject(mainObj1[key]) && _.isObject(mainObj2[key]))
+      return {
+        key,
+        children: makeAstTree(mainObj1[key], mainObj2[key]),
+        status: 'nested',
+      };
+    if (mainObj1[key] != mainObj2[key])
+      return { key, newValue: mainObj2[key], oldValue: mainObj1[key], status: 'updated'};
+  });
+  return treePart;
+};
+
+export default makeAstTree;
